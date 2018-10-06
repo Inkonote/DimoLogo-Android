60636ea68286b04d94467615603e59f682ebbe86
luckytianyiyan <luckytianyiyan@gmail.com>
2018-10-07 05:47:16 +0800

    Add Extensions

---
+++ dimologo/src/main/java/sh/tyy/dimo/logo/Extension.kt
@@ -0,0 +1,20 @@
+package sh.tyy.dimo.logo
+
+import android.graphics.Path
+import android.util.Pair
+
+internal fun Path.moveTo(point: Pair<Float, Float>) {
+    moveTo(point.first, point.second)
+}
+
+internal fun Path.lineTo(point: Pair<Float, Float>) {
+    lineTo(point.first, point.second)
+}
+
+internal fun Path.quadTo(controlPointX: Float, controlPointY: Float, endPoint: Pair<Float, Float>) {
+    quadTo(controlPointX, controlPointY, endPoint.first, endPoint.second)
+}
+
+internal fun ClosedFloatingPointRange<Float>.length(): Float {
+    return endInclusive - start
+}
