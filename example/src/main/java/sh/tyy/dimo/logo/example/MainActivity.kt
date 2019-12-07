e2ce5fc11fb0cad61566fda80b20450071d3dd3c
ty0x2333 <luckytianyiyan@gmail.com>
2019-12-07 10:53:20 +0800

    Optimize Code

--- example/src/main/java/sh/tyy/dimo/logo/example/MainActivity.kt
+++ example/src/main/java/sh/tyy/dimo/logo/example/MainActivity.kt
@@ -2,7 +2,7 @@ package sh.tyy.dimo.logo.example
 
 import android.app.Dialog
 import android.os.Build
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
